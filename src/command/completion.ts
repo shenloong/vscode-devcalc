339ca3375aeff856708000639d3e1ca3b4a05948
Shen Zhilong <shenzhilong0921@foxmail.com>
2024-04-08 16:57:45 +0800

    Update

--- src/command/completion.ts
+++ src/command/completion.ts
@@ -1,38 +1,22 @@
-import { CompletionItem, CompletionItemKind, CompletionItemProvider, TextDocument, Position } from 'vscode';
-import { config } from './configuration';
-import { px2dev } from './conversion';
+import { CompletionItem, CompletionItemKind, CompletionItemProvider, TextDocument, Position } from 'vscode'
+import { config } from './configuration'
+import { px2dev } from './conversion'
 
-export const px2devCompletion: CompletionItemProvider<CompletionItem> = {
+const pxPattern = /(-?[0-9]*.?[0-9]+)p(x)?/
+
+export class Completion implements CompletionItemProvider {
     provideCompletionItems(document: TextDocument, position: Position) {
-        if (!config.enableAutocompleteConversion) return;
-        return new Promise((resolve) => {
-            const wordRange = document.getWordRangeAtPosition(position, /([-]?[\d.]+)p(x)?/);
-            const results = convert(document.getText(wordRange));
-            if (results.length === 0) return resolve([]);
-            const result = results.map(item => {
-                // const completionItem = new CompletionItem(item.label, CompletionItemKind.Value);
-                const completionItem = new CompletionItem(item.label, CompletionItemKind.Event);
-                completionItem.insertText = item.devText;
-                completionItem.preselect = true;
-                return completionItem;
-            });
-            return resolve(result);
-        });
+        if (!config.enableAutocompleteConversion) return []
+        const result = convert(document.getText(document.getWordRangeAtPosition(position, pxPattern)))
+        if (!result) return []
+        const item = new CompletionItem(result.label, CompletionItemKind.Event)
+        // item.kind = CompletionItemKind.Value
+        item.insertText = result.devText
+        return [item]
     }
-};
-
-interface ConvertResult {
-    pxValue: number;
-    pxText: string;
-    devValue: number;
-    devText: string;
-    label: string;
 }
 
 function convert(text: string) {
-    const result: ConvertResult[] = [];
-    const match = text.match(/([-]?[\d.]+)p(x)?/);
-    if (!match) return result;
-    result.push(px2dev(match[1]));
-    return result;
+    const match = text.match(pxPattern)
+    return match ? px2dev(match[1]) : null
 }
